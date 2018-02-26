40573c7e88839ea4ce98584fb490712495d51c0b
Shrikkanth Ramesh <shrikkanth.ramesh@unicosolution.com>
2018-02-26 11:40:24 -0600

    Template Created
    First Commit

---
+++ src/main/java/com/foodscribe/FoodscribeApplication.java
@@ -0,0 +1,12 @@
+package com.foodscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodscribeApplication.class, args);
+	}
+}
